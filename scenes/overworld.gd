6c2c20a865c2817c4f1df8b48e2f1fc9ef812665
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2018-02-25 18:49:29 -0800

    Added a camera that moves screens
    Made a camera that moves to a new screen when the player moves outside
    of it. Also added a tilemap to the overworld

---
+++ scenes/overworld.gd
@@ -0,0 +1,27 @@
+extends Node
+
+onready var player = get_node("player")
+onready var camera = get_node("camera")
+onready var camera_tween = get_node("camera/camera_tween")
+
+var screensize
+
+func _ready():
+	screensize = get_viewport().size
+	set_physics_process(true)
+
+func _physics_process(delta):
+	move_camera()
+	
+func move_camera():
+	var x = player.position.x
+	var y = player.position.y
+	if x < 0:
+		x -= screensize.x
+	if y < 0:
+		y -= screensize.y
+	var new_x = int(x / screensize.x) * screensize.x
+	var new_y = int(y / screensize.y) * screensize.y
+	var new_pos = Vector2(new_x, new_y)
+	camera_tween.interpolate_property(camera, "position", camera.position, new_pos, 0.001, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	camera_tween.start()
